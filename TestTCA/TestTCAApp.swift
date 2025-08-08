c995bb6813e71d11cbafabafe0abb7485706c887
stone <stoneyanjun@gmail.com>
2025-08-08 16:21:12 +0800

    init

---
+++ TestTCA/TestTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTCAApp.swift
+//  TestTCA
+//
+//  Created by stone on 2025/8/8.
+//
+
+import SwiftUI
+
+@main
+struct TestTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
